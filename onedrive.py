673edd2fd338cc21e600869b3d25a581e19b43ef
Chris Lowe <csl619@outlook.com>
2021-08-04 14:41:45 +0100

    rework onedrive process for changes relating to url lookups

--- onedrive.py
+++ onedrive.py
@@ -15,22 +15,16 @@ load_dotenv(dotenv_path)
 
 
 class Onedrive:
-    def __init__(
-            self, todays_folder, file, backup_folder, site_url, drive_id,
-            tenant_id, client_id, client_secret, redirect):
-        self.todays_folder = todays_folder
-        self.backup_file = file
-        self.backup_folder = backup_folder
-        self.site_url = site_url
-        self.drive_id = drive_id
+    def __init__(self, tenant_id, client_id, client_secret, redirect):
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
         self.redirect = redirect
         self.base_url = 'https://graph.microsoft.com/v1.0/'
-        self.backup_path = (
-            f'{self.base_url}/{self.site_url}/drives/{self.drive_id}/root:')
         self.token = self.__token()
+        self.headers = {
+            'Authorization': "Bearer " + self.token,
+            }
 
     def __token(self):
         data = {'grant_type': "client_credentials",
@@ -48,33 +42,61 @@ class Onedrive:
         else:
             return 'ERROR'
 
+    def site(self, site_name):
+        self.site_name = site_name
+        URL = f'{self.base_url}sites?search={self.site_name}'
+        r = requests.get(
+            URL, headers=self.headers)
+        site = json.loads(r.text)
+        self.site_id = site['value'][0]['id']
+
+    def drive(self):
+        URL = f'{self.base_url}sites/{self.site_id}/drive'
+        r = requests.get(
+            URL, headers=self.headers)
+        drive = json.loads(r.text)
+        self.drive_id = drive['id']
+
+    def check_folder_exists(self, backup_folder):
+        self.backup_folder = backup_folder
+        URL = f'{self.base_url}drives/{self.drive_id}/root:/'
+        r = requests.get(
+            URL + self.backup_folder, headers=self.headers)
+        if not r.ok:
+            post_url = f'{self.base_url}drives/{self.drive_id}/root/children'
+            headers = self.headers
+            headers['Content-type'] = 'application/json'
+            data = {
+                "name": self.backup_folder,
+                "folder": {}
+            }
+            r = requests.post(post_url, headers=headers, json=data)
+            print(f'Backup folder created for database: {self.backup_folder}')
+        else:
+            print(f'Backup folder already exists for: {self.backup_folder}')
+
     def __requests_retry_session(
-        self,
-        retries=3,
-        backoff_factor=0.3,
-        status_forcelist=(500, 502, 504),
-        session=None,
-    ):
+            self, retries=3, backoff_factor=0.3,
+            status_forcelist=(500, 502, 504), session=None):
         session = session or requests.Session()
         retry = Retry(
-            total=retries,
-            read=retries,
-            connect=retries,
-            backoff_factor=backoff_factor,
-            status_forcelist=status_forcelist,
-        )
+            total=retries, read=retries, connect=retries,
+            backoff_factor=backoff_factor, status_forcelist=status_forcelist)
         adapter = HTTPAdapter(max_retries=retry)
         session.mount('http://', adapter)
         session.mount('https://', adapter)
         return session
 
-    def upload(self):
-        directory = rf"/backup/dbbackup/{self.todays_folder}"
-        URL = f"{self.backup_path}/{self.backup_folder}/{self.todays_folder}"
-        headers = {
-            'Authorization': "Bearer " + self.token,
-            'Content-Type': 'application/gzip'
-            }
+    def upload(self, backup_folder_path, todays_folder, file):
+        self.backup_folder_path = backup_folder_path
+        self.todays_folder = todays_folder
+        self.backup_file = file
+        directory = rf"{self.backup_folder_path}/{self.todays_folder}"
+        URL = (
+            f"{self.base_url}drives/{self.drive_id}/root:/"
+            f"{self.backup_folder}/{self.todays_folder}")
+        headers = self.headers
+        headers['Content-Type'] = 'application/gzip'
         print("Uploading file(s) to " + URL)
         for root, dirs, files in os.walk(directory):
             filepath = os.path.join(root, self.backup_file)
@@ -94,12 +116,11 @@ class Onedrive:
             fileHandle.close()
 
     def delete_files(self):
-        URL = f"{self.backup_path}/{self.backup_folder}"
-        headers = {
-            'Authorization': "Bearer " + self.token,
-            }
+        URL = (
+            f"{self.base_url}drives/{self.drive_id}/root:/"
+            f"{self.backup_folder}")
         r = requests.get(
-            URL+':/children', headers=headers)
+            URL+':/children', headers=self.headers)
         j = json.loads(r.text)
         for i in j['value']:
             today = datetime.now().replace(
@@ -109,63 +130,25 @@ class Onedrive:
             start_date = (today - timedelta(days=7))
             if created < start_date:
                 del_path = f"{self.base_url}/drive/items/{i['id']}"
-                del_ret = requests.delete(del_path, headers=headers)
+                del_ret = requests.delete(del_path, headers=self.headers)
                 print(del_ret)
                 print('Folder Removed')
-
-    def check_folder_exists(self):
-        headers = {
-            'Authorization': "Bearer " + self.token,
-            'if-none-match': ''
-            }
-        URL = f'{self.base_url}/{self.site_url}/drives/{self.drive_id}/root:/'
-        r = requests.get(
-            URL + self.backup_folder, headers=headers)
-        if not r.ok:
-            post_url = (
-                f'{self.base_url}/{self.site_url}/drives/{self.drive_id}/'
-                f'root/children')
-            headers['Content-type'] = 'application/json'
-            data = {
-                "name": self.backup_folder,
-                "folder": {}
-            }
-            r = requests.post(post_url, headers=headers, json=data)
-
-    def discord_post(self, response, user, webhook):
-        if response == 'ERROR':
-            message = (
-                f'Database backup failed, please check logs. <@&{user}>')
-        else:
-            message = 'Database backup completed successfully.'
-        content = {
-            'username': 'SupportBot',
-            'content': message,
-        }
-        webhook = requests.post(webhook, data=content)
-        if webhook.ok:
-            print('Discord Message Sent')
-        else:
-            print(webhook.json())
 
 
 if __name__ == '__main__':
     onedrive = Onedrive(
-        todays_folder=sys.argv[1:][0], file=sys.argv[1:][1],
-        backup_folder=getenv('DB_BACKUP_FOLDER'),
-        site_url=getenv('OD_SITE_URL'), drive_id=getenv('OD_DRIVE_ID'),
-        tenant_id=getenv('OD_TENANT_ID'), client_id=getenv('OD_CLIENT_ID'),
+        tenant_id=getenv('OD_TENANT_ID'),
+        client_id=getenv('OD_CLIENT_ID'),
         client_secret=getenv('OD_CLIENT_SECRET'),
         redirect=getenv('OD_REDIRECT'),
         )
     if onedrive.token != "ERROR":
-        onedrive.check_folder_exists()
-        onedrive.upload()
+        onedrive.site(site_name=getenv('OD_SITE_NAME'))
+        onedrive.drive()
+        onedrive.check_folder_exists(backup_folder=getenv('DB_BACKUP_FOLDER'))
+        onedrive.upload(
+            backup_folder_path=getenv('DB_BACKUP_FOLDER_PATH'),
+            todays_folder=sys.argv[1:][0], file=sys.argv[1:][1],)
         onedrive.delete_files()
-        onedrive.discord_post(
-            response="SUCCESS", user=getenv('WH_USER'),
-            webhook=getenv('BACKUP_WH'))
     else:
-        onedrive.discord_post(
-            response="ERROR", user=getenv('WH_USER'),
-            webhook=getenv('BACKUP_WH'))
+        print("Error incorrect Onedrive token, cannot continue")
