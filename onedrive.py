73aba214aa7d8a75c96b00c5560316f737bd3df7
Chris Lowe <csl619@outlook.com>
2021-08-05 11:17:54 +0100

    move requests into own private functions and add print statements

--- onedrive.py
+++ onedrive.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 from os.path import join, dirname
 from os import getenv
 from dotenv import load_dotenv
+from pprint import pprint
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -35,45 +36,57 @@ class Onedrive:
                 'redirect_uri': self.redirect}
         URL = (
             f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/token")
-        token_r = requests.post(URL, data=data)
-        if token_r.ok:
-            TOKEN = token_r.json().get('access_token')
-            return TOKEN
+        token = self.__post(URL, data)
+        if token:
+            return token['access_token']
         else:
             return 'ERROR'
 
     def site(self, site_name):
+        print("Getting Site ID...")
         self.site_name = site_name
         URL = f'{self.base_url}sites?search={self.site_name}'
-        r = requests.get(
-            URL, headers=self.headers)
-        site = json.loads(r.text)
-        self.site_id = site['value'][0]['id']
+        site = self.__get(URL)
+        if len(site['value']) > 0:
+            self.site_id = site['value'][0]['id']
+            print(f'---> Site found: {self.site_id}')
+            self.__drive()
+        else:
+            print(f'---> Site {self.site_name} could not be found.')
 
-    def drive(self):
+    def __drive(self):
+        print("Getting Drive ID...")
         URL = f'{self.base_url}sites/{self.site_id}/drive'
-        r = requests.get(
-            URL, headers=self.headers)
-        drive = json.loads(r.text)
-        self.drive_id = drive['id']
+        drive = self.__get(URL)
+        if drive:
+            self.drive_id = drive['id']
+            print(f'---> Drive found: {self.drive_id}')
+        else:
+            print('---> Drive could not be found within your specified Site.')
 
     def check_folder_exists(self, backup_folder):
+        print("Checking if backup folder exists...")
         self.backup_folder = backup_folder
-        URL = f'{self.base_url}drives/{self.drive_id}/root:/'
-        r = requests.get(
-            URL + self.backup_folder, headers=self.headers)
-        if not r.ok:
-            post_url = f'{self.base_url}drives/{self.drive_id}/root/children'
-            headers = self.headers
-            headers['Content-type'] = 'application/json'
-            data = {
-                "name": self.backup_folder,
-                "folder": {}
-            }
-            r = requests.post(post_url, headers=headers, json=data)
-            print(f'Backup folder created for database: {self.backup_folder}')
+        URL = (
+            f'{self.base_url}drives/{self.drive_id}/root:/'
+            f'{self.backup_folder}')
+        r = self.__get(URL)
+        if not r:
+            self.__create_folder()
         else:
-            print(f'Backup folder already exists for: {self.backup_folder}')
+            print(
+                f'---> Backup folder already exists for: {self.backup_folder}')
+
+    def __create_folder(self):
+        url = f'{self.base_url}drives/{self.drive_id}/root/children'
+        headers = self.headers
+        headers['Content-type'] = 'application/json'
+        data = {
+            "name": self.backup_folder,
+            "folder": {}
+        }
+        self.__post(url, json.dumps(data))
+        print(f'---> Backup folder created for database: {self.backup_folder}')
 
     def __requests_retry_session(
             self, retries=3, backoff_factor=0.3,
@@ -88,6 +101,7 @@ class Onedrive:
         return session
 
     def upload(self, backup_folder_path, todays_folder, file):
+        print("Uploading backup file...")
         self.backup_folder_path = backup_folder_path
         self.todays_folder = todays_folder
         self.backup_file = file
@@ -97,42 +111,66 @@ class Onedrive:
             f"{self.backup_folder}/{self.todays_folder}")
         headers = self.headers
         headers['Content-Type'] = 'application/gzip'
-        print("Uploading file(s) to " + URL)
+        print("---> Backup files uploading to " + URL)
         for root, dirs, files in os.walk(directory):
             filepath = os.path.join(root, self.backup_file)
-            print("Uploading "+self.backup_file+"....")
+            print("Uploading "+self.backup_file+"...")
             fileHandle = open(filepath, 'rb')
             try:
                 r = self.__requests_retry_session().put(
                     URL+"/"+self.backup_file+":/content", data=fileHandle,
                     headers=headers)
             except Exception as x:
-                print("Script error")
+                print("---> Upload failed")
                 print(x)
                 print(r.status_code, r.text)
-                self.discord_post('ERROR')
             else:
-                print("Script completed")
+                print("---> Upload completed")
             fileHandle.close()
 
     def delete_files(self):
         URL = (
             f"{self.base_url}drives/{self.drive_id}/root:/"
-            f"{self.backup_folder}")
-        r = requests.get(
-            URL+':/children', headers=self.headers)
-        j = json.loads(r.text)
-        for i in j['value']:
-            today = datetime.now().replace(
-                hour=0, minute=0, second=0, microsecond=0)
-            created = datetime.strptime(
-                i['createdDateTime'], "%Y-%m-%dT%H:%M:%SZ")
-            start_date = (today - timedelta(days=7))
-            if created < start_date:
-                del_path = f"{self.base_url}/drive/items/{i['id']}"
-                del_ret = requests.delete(del_path, headers=self.headers)
-                print(del_ret)
-                print('Folder Removed')
+            f"{self.backup_folder}:/children")
+        r = self.__get(URL)
+        if r:
+            for i in r['value']:
+                today = datetime.now().replace(
+                    hour=0, minute=0, second=0, microsecond=0)
+                created = datetime.strptime(
+                    i['createdDateTime'], "%Y-%m-%dT%H:%M:%SZ")
+                start_date = (today - timedelta(days=7))
+                if created < start_date:
+                    print(f"Deleting folder {i['name']}")
+                    delete_path = f"{self.base_url}/drive/items/{i['id']}"
+                    del_ret = self.__delete(delete_path)
+                    print(del_ret)
+                    print('---> Folder removed')
+
+    def __get(self, url):
+        result = requests.get(url, headers=self.headers)
+        if result.ok:
+            return json.loads(result.text)
+        else:
+            return result.ok
+
+    def __post(self, url, data):
+        if hasattr(self, 'headers'):
+            headers = self.headers
+        else:
+            headers = None
+        result = requests.post(url, headers=headers, data=data)
+        if result.ok:
+            return json.loads(result.text)
+        else:
+            return result.ok
+
+    def __delete(self, url):
+        result = requests.delete(url, headers=self.headers)
+        if result.ok:
+            return result.text
+        else:
+            return result.ok
 
 
 if __name__ == '__main__':
@@ -144,11 +182,16 @@ if __name__ == '__main__':
         )
     if onedrive.token != "ERROR":
         onedrive.site(site_name=getenv('OD_SITE_NAME'))
-        onedrive.drive()
-        onedrive.check_folder_exists(backup_folder=getenv('DB_BACKUP_FOLDER'))
-        onedrive.upload(
-            backup_folder_path=getenv('DB_BACKUP_FOLDER_PATH'),
-            todays_folder=sys.argv[1:][0], file=sys.argv[1:][1],)
-        onedrive.delete_files()
+        if hasattr(onedrive, 'drive_id'):
+            onedrive.check_folder_exists(
+                backup_folder=getenv('DB_BACKUP_FOLDER'))
+            onedrive.upload(
+                backup_folder_path=getenv('DB_BACKUP_FOLDER_PATH'),
+                todays_folder=sys.argv[1:][0], file=sys.argv[1:][1],)
+            onedrive.delete_files()
+        else:
+            print(
+                f"No Site found for: {getenv('OD_SITE_NAME')}, "
+                f"cannot continue")
     else:
-        print("Error incorrect Onedrive token, cannot continue")
+        print("Error invalid Onedrive token, cannot continue")
